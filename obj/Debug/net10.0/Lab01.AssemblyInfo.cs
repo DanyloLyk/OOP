4e41c71f17211249fe817f8bf0532694b465483c
DanyloLyk <danylolyk@gmail.com>
2026-09-23 13:04:15 +0300

    add readme.md file

--- obj/Debug/net10.0/Lab01.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fa2bc076b81f1062c209de750a2d02fac78503")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
